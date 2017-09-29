27e250bda6c180310cc478379bbba6ce0f0e1bc0
superingxz <1803117759@qq.com>
2017-09-29 15:39:20 +0800

    1.Resolving memory leaks

--- arms/src/main/java/com/jess/arms/mvp/BasePresenter.java
+++ arms/src/main/java/com/jess/arms/mvp/BasePresenter.java
@@ -21,6 +21,8 @@ import com.trello.rxlifecycle2.RxLifecycle;
 
 import org.simple.eventbus.EventBus;
 
+import java.lang.ref.WeakReference;
+
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
 
@@ -39,8 +41,9 @@ public class BasePresenter<M extends IModel, V extends IView> implements IPresen
     protected CompositeDisposable mCompositeDisposable;
 
     protected M mModel;
-    protected V mRootView;
+    //protected V mRootView;
 
+    protected WeakReference<V> mView;
     /**
      * 如果当前页面同时需要 Model 层和 View 层,则使用此构造函数(默认)
      *
@@ -49,7 +52,8 @@ public class BasePresenter<M extends IModel, V extends IView> implements IPresen
      */
     public BasePresenter(M model, V rootView) {
         this.mModel = model;
-        this.mRootView = rootView;
+        //this.mRootView = rootView;
+        this.mView = new WeakReference<V>(rootView);
         onStart();
     }
 
@@ -59,7 +63,8 @@ public class BasePresenter<M extends IModel, V extends IView> implements IPresen
      * @param rootView
      */
     public BasePresenter(V rootView) {
-        this.mRootView = rootView;
+        //this.mRootView = rootView;
+        this.mView = new WeakReference<V>(rootView);
         onStart();
     }
 
@@ -85,7 +90,8 @@ public class BasePresenter<M extends IModel, V extends IView> implements IPresen
         if (mModel != null)
             mModel.onDestroy();
         this.mModel = null;
-        this.mRootView = null;
+        //this.mRootView = null;
+        mView.clear();
         this.mCompositeDisposable = null;
     }
 
